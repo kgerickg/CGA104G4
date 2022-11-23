562d16875cfc922f3c470a13c14d3f4d178398f3
JimmyLin <notoriousnovel@gmail.com>
2022-11-24 00:42:20 +0800

    優惠活動後台修改刪除完成，新增與複製功能待完成

--- CGA104G4/src/main/java/com/promo/model/PromoDAO_interface.java
+++ CGA104G4/src/main/java/com/promo/model/PromoDAO_interface.java
@@ -2,7 +2,11 @@ package com.promo.model;
 
 import com.basicDAO.BasicDAO_interface;
 
+import java.sql.Timestamp;
+
 public interface PromoDAO_interface extends BasicDAO_interface<PromoVO> {
+
+    public Integer checkTime(Timestamp promoTime);
 
 
 }
--- CGA104G4/src/main/java/com/promo/model/PromoService.java
+++ CGA104G4/src/main/java/com/promo/model/PromoService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.sql.Timestamp;
 import java.util.List;
 
 @Service
@@ -20,4 +21,41 @@ public class PromoService {
     public void delete(Integer promoId) {
 		dao.delete(promoId);
     }
+
+    public boolean[] checkTime(Timestamp promoTimeS, Timestamp promoTimeE,Integer promoId) {
+		boolean[] result = {false,false};
+		Integer checkIdS = dao.checkTime(promoTimeS);
+		Integer checkIdE = dao.checkTime(promoTimeE);
+		if(checkIdS==null||checkIdS==promoId){
+			result[0] = true;
+		}
+		if(checkIdE==null||checkIdE==promoId){
+			result[1] = true;
+		}
+		return result;
+    }
+
+	public boolean[] checkTime(Timestamp promoTimeS, Timestamp promoTimeE) {
+		boolean[] result = {false,false};
+		Integer checkIdS = dao.checkTime(promoTimeS);
+		Integer checkIdE = dao.checkTime(promoTimeE);
+		if(checkIdS==null){
+			result[0] = true;
+		}
+		if(checkIdE==null){
+			result[1] = true;
+		}
+		return result;
+	}
+	@Transactional
+	public void update(Integer promoId, String promoName, Integer promoVal, Timestamp promoTimeS, Timestamp promoTimeE, String promoCont) {
+		PromoVO promoVO = new PromoVO();
+		promoVO.setPromoId(promoId);
+		promoVO.setPromoName(promoName);
+		promoVO.setPromoVal(promoVal);
+		promoVO.setPromoTimeS(promoTimeS);
+		promoVO.setPromoTimeE(promoTimeE);
+		promoVO.setPromoCont(promoCont);
+		dao.update(promoVO);
+	}
 }
